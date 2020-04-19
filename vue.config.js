bf4573da2c996703a7eac089d042f79201da30e6
oussama <oussamajelliti@gmail.com>
2020-04-19 18:20:36 +0200

    Add webpack plugin to inject Tw/Fb meta tags to index.html

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 'use strict';
+const SocialTags = require('social-tags-webpack-plugin')
 module.exports = {
     pwa: {
         appleMobileWebAppCapable: 'yes',
@@ -6,6 +7,33 @@ module.exports = {
         appleMobileWebAppStatusBarStyle: 'black',
         iconPaths :[
 
+        ]
+    },
+    configureWebpack: {
+        plugins: [
+            new SocialTags({
+                appUrl: 'http://player1.jelliti.me/',
+                facebook: {
+                    //'fb:app_id': "",
+                    'og:url': "http://player1.jelliti.me/",
+                    'og:type': "music.radio_station",
+                    'og:title': "JoujmaFM player",
+                    'og:image': 'src/img/book.png',
+                    'og:description': "JoujmaFM player, 24h/7 of music",
+                    'og:site_name': "JoujmaFM",
+                    'og:locale': "FR_fr",
+                    'og:article:author': "simyos",
+                },
+                twitter: {
+                    "twitter:card": "summary",
+                    "twitter:site": "@site_account",
+                    "twitter:creator": "@individual_account",
+                    "twitter:url": "http://example.com/page.html",
+                    "twitter:title": "JoujmaFM player",
+                    "twitter:description": "JoujmaFM player, 24h/7 of music",
+                    "twitter:image": '<%= BASE_URL %>/img/icon.png'
+                },
+            })
         ]
     },
     productionSourceMap : false,
