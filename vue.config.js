999818b866d9ddc031272a9f7f726b8034c62356
oussamatn <oussama@jelliti.me>
2021-05-09 23:27:11 +0200

    fix handling at npm build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 'use strict';
-const SocialTags = require('social-tags-webpack-plugin')
-//const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+//const SocialTags = require('social-tags-webpack-plugin')
 const config = require('./public/assets/config.json')
+
 module.exports = {
     pwa: {
         appleMobileWebAppCapable: 'yes',
@@ -15,27 +15,27 @@ module.exports = {
         plugins: [
             //² new FaviconsWebpackPlugin('src/asserts/logo.png'), // svg works too!
 
-            new SocialTags({
-                appUrl: config.meta.url,
-                facebook: {
-                    //'fb:app_id': "",
-                    'og:url': config.meta.url,
-                    'og:type': "music.radio_station",
-                    'og:title': config.title,
-                    'og:image': config.meta.image,
-                    'og:description': config.description,
-                    'og:site_name': config.title,
-                    'og:locale': config.meta.locale,
-                },
-                twitter: {
-                    "twitter:card": "summary",
-                    "twitter:site": config.twitter.username,
-                    "twitter:url": config.meta.url,
-                    "twitter:title": config.title,
-                    "twitter:description": config.description,
-                    "twitter:image": config.meta.image
-                },
-            })
+            // new SocialTags({
+            //     // appUrl: config.meta.url,
+            //     // facebook: {
+            //     //     //'fb:app_id': "",
+            //     //     'og:url': config.meta.url,
+            //     //     'og:type': "music.radio_station",
+            //     //     'og:title': config.title,
+            //     //     'og:image': config.meta.image,
+            //     //     'og:description': config.description,
+            //     //     'og:site_name': config.title,
+            //     //     'og:locale': config.meta.locale,
+            //     // },
+            //     // twitter: {
+            //     //     "twitter:card": "summary",
+            //     //     "twitter:site": config.twitter.username,
+            //     //     "twitter:url": config.meta.url,
+            //     //     "twitter:title": config.title,
+            //     //     "twitter:description": config.description,
+            //     //     "twitter:image": config.meta.image
+            //     // },
+            // })
         ]
     },
     productionSourceMap : false,
