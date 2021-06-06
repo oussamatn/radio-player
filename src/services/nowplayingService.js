d9dc48bc05b040bf50794aba251f9e6a2558894b
oussamatn <oussama@jelliti.me>
2021-06-07 00:08:54 +0200

    Fix API path

--- src/services/nowplayingService.js
+++ src/services/nowplayingService.js
@@ -7,7 +7,7 @@ import config from '../../public/assets/config.json';
 export default {
     // get now playing response
     get(){
-        let apiurl = config.api_url+'/api/nowplaying';
+        let apiurl = config.api_url+'/nowplaying';
         //let error  = 'There was a problem fetching the Now Playing API from JoujmaFM.';
         return axios.get( apiurl ).then( res => {
             const list = this._parseChannels( res.data );
@@ -17,7 +17,7 @@ export default {
     },
     // get channels data from api
     getChannels(  ) {
-        let apiurl = config.api_url+'/api/stations';
+        let apiurl = config.api_url+'/stations';
         //let error  = 'There was a problem fetching the latest list of channels from JoujmaFM.';
         return axios.get( apiurl ).then( res => {
             const list = this._parseChannels( res.data );
@@ -37,7 +37,7 @@ export default {
     // fetch songs for a channel
     getSongs( channel_id ) {
         console.log("Nowplaying Service :  getSongs : ", channel_id)
-        let apiurl =   config.api_url+'/api/nowplaying/'+ channel_id;
+        let apiurl =   config.api_url+'/nowplaying/'+ channel_id;
         return axios.get( apiurl ).then( res => {
             //if ( !res.data ) return callback( error, [] );
             res.data.station = this._parseChannel( res.data.station );
@@ -49,8 +49,8 @@ export default {
     // parse channels list from api response
     _parseChannel( c ) {
                 c.mp3file   = c.listen_url;
-                c.image     = '/img/'+c.shortcode+'.png';
-                c.songsurl  = config.api_url+'/api/nowplaying/'+ c.id;
+                c.image     =   '/img/icon.png' || '/img/stations/'+c.shortcode+'.png' ;
+                c.songsurl  = config.api_url+'/nowplaying/'+ c.id;
                 c.twitter   = 'https://twitter.com/@';
                 c.route     = '/station/'+ c.shortcode;
                 c.favorite  = false;
@@ -61,13 +61,13 @@ export default {
     _parseChannels( channels ) {
         let output = [];
 
-        //if ( Array.isArray( channels ) ) {
+
             console.log("parchannels for loop ")
             for ( let ch of channels ) {
                 let c = ch.station;
                 c.mp3file   = c.listen_url;
                 c.image     = '/img/'+c.shortcode+'.png';
-                c.songsurl  = config.api_url+'/api/nowplaying/'+ c.id;
+                c.songsurl  = config.api_url+'/nowplaying/'+ c.id;
                 c.twitter   = 'https://twitter.com/@';
                 c.route     = '/channel/'+ c.shortcode;
                 c.favorite  = false;
@@ -75,7 +75,7 @@ export default {
                 ch.station = c;
                 output.push( ch );
             }
-       //}
+
 
         return output;
     },
